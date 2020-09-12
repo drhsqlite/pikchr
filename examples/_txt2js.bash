71f72ac98b699a203d0d57213a41a1e3003871dc
stephan <sgbeal@googlemail.com>
2020-09-12 19:25:52 +0000

    Added examples/_txt2js.bash to convert .txt scripts to something usable by Fossil's pikchrshow page's example/currated scripts. A minor syntactic tweak to objects.txt was necessary for that conversion to work: pikchr lines which end in a backslash need a space before that backslash or JS will end up combining the 2 lines with no space between them, leading to a pikchr syntax error.
    FossilOrigin-Name: e42e629b7681e22acd3e9d915c1873f4b7f71521e8aa73aef0584b9614edf3a5

---
+++ examples/_txt2js.bash
@@ -0,0 +1,81 @@
+#!/bin/bash
+########################################################################
+# Converts pikchr-format .txt files into a form usable by the
+# Fossil SCM's pikchrshow page's "example scripts" JS code.
+#
+# Usage: $0 [options] [file1.txt [...fileN.txt]]
+#
+# Options:
+#
+#  -o outfile, defaulting to /dev/stdout
+#
+# Its list of files defaults to $(ls -1 *.txt | sort).
+########################################################################
+
+function die(){
+    local rc=$1
+    shift
+    echo "$@" 1>&2
+    exit $rc
+}
+
+optOutfile=/dev/stdout
+scriptList=()
+while [[ x != "x$1" ]]; do
+    case "$1" in
+        -o) shift
+            optOutfile="$1"
+            shift
+            [[ x = "x${optOutfile}" ]] && die 1 "Missing filename for -o arg."
+            ;;
+        *) scriptList+=("$1")
+           shift
+    esac
+done
+
+[[ 0 = ${#scriptList[@]} ]] && {
+    scriptList=( $(ls -1 *.txt | sort) )
+    [[ 0 = ${#scriptList[@]} ]] && die 1 "Cannot find any *.txt files."
+}
+
+########################################################################
+# Optional *brief* user-friendly descriptive name of test files, in
+# the form desc_TESTNAME="name", where TESTNAME is the base filename
+# part of an input file. The default friendly name is that base
+# filename. These names are the ones shown in pikchrshow's example
+# script selection list.
+desc_objects="Core object types"
+desc_swimlane="Swimlanes"
+desc_headings01="Cardinal headings"
+########################################################################
+
+#echo "scriptList=${scriptList[@]}"
+#echo optOutfile=${optOutfile}
+########################################################################
+# Output 1 JS object per file, comma-separated:
+#
+#  {name: "filename or desc_BASENAME value",
+#   code: `file content`}
+#
+# Note that the output is intended for embedding in an array but does
+# not emit the [...] part itself because of how its output is used.
+{
+    n=0 # object count
+    for f in ${scriptList[@]}; do
+        [[ -f "$f" ]] || die $? "Missing file: $f"
+        fb=${f%%.txt}
+        fb=${fb##*/}
+        descVar=desc_${fb}
+        desc=${!descVar}
+        [[ x = "x${desc}" ]] && desc="$fb"
+        #echo f=${f} fb=${fb} descV=${descV} desc=$desc
+        [[ $n -gt 0 ]] && echo -n ","
+        echo -n '{name:"'${desc}'",'
+        echo -n 'code:`'
+        cat $f
+        echo -n '`}'
+        n=$((n + 1))
+    done
+    echo
+} > "${optOutfile}"
+#echo "Done: ${n} file(s) processes. Output is in ${optOutfile}."
