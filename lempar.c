bece23c4bb357ee9a07bae977a2db27ec9257d5c
drh <>
2020-09-20 13:34:02 +0000

    Update the built-in Lemon code to the latest from the SQLite project.
    FossilOrigin-Name: ddf77efde148e4334e8e4284c1640d86bc5f7f3a7deb34fe8356de4dbc725072

--- lempar.c
+++ lempar.c
@@ -718,8 +718,9 @@ static YYACTIONTYPE yy_reduce(
   (void)yyLookahead;
   (void)yyLookaheadToken;
   yymsp = yypParser->yytos;
+  assert( yyruleno<(int)(sizeof(yyRuleName)/sizeof(yyRuleName[0])) );
 #ifndef NDEBUG
-  if( yyTraceFILE && yyruleno<(int)(sizeof(yyRuleName)/sizeof(yyRuleName[0])) ){
+  if( yyTraceFILE ){
     yysize = yyRuleInfoNRhs[yyruleno];
     if( yysize ){
       fprintf(yyTraceFILE, "%sReduce %d [%s]%s, pop back to state %d.\n",
